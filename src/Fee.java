386fb1196729fd21d061599eb7a57189bd7308f7
tomirisospanova <124910398+tomirisospanova@users.noreply.github.com>
2023-02-10 10:25:31 +0600

    Update Fee.java

--- src/Fee.java
+++ src/Fee.java
@@ -6,8 +6,13 @@ public class Fee {
         this.lateFee = lateFee;
     }
 
-    public int calculateTotalFee(int months) {
-        int totalFee = monthlyFee * months;        if (months > 12) {
-            totalFee += lateFee * (months - 12);        }
-        return totalFee;    }
+    public int calculateTotalFee(int months) 
+    {
+        int totalFee = monthlyFee * months;    
+        if (months > 12) 
+        {
+            totalFee += lateFee * (months - 12);       
+        }
+        return totalFee;   
+    }
 }
