964ac699026039fdad84e3271b93bf11dd484175
nasredd1n <122224358+nasredd1n@users.noreply.github.com>
2023-02-13 21:18:29 +0600

    Update RoomRepository.java

--- src/repositories/RoomRepository.java
+++ src/repositories/RoomRepository.java
@@ -14,3 +14,28 @@ public class RoomRepository implements RoomsRepository {
     }
 
 }
+public Room getRoom(int id){
+        Connection con = null;
+        try {
+            con = db.getConnection();
+            String sql = "SELECT id, roomNumber, numberOfBeds, type";
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if(rs.next()){
+                Room room = new Room(rs.getInt("id"), rs.getInt("roomNumber"), rs.getInt("numberOfBeds"), rs.getString("type"));
+                return room;
+            }
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                con.close();
+            }catch (SQLException throwables){
+                throwables.printStackTrace();
+            }
+        }
+        return null;
+    }
