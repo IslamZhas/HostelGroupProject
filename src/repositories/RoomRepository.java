2ceb4fd9ebdc1345a683d5faa508911efefc462b
tomirisospanova <124910398+tomirisospanova@users.noreply.github.com>
2023-02-14 11:06:26 +0600

    Update RoomRepository.java

--- src/repositories/RoomRepository.java
+++ src/repositories/RoomRepository.java
@@ -14,6 +14,32 @@ public class RoomRepository implements RoomsRepository {
     }
 
 }
+public List<Room> getAllRooms(){
+        Connection con = null;
+        try{
+            con = db.getConnection();
+            String sql = "SELECT id, roomNumber, numberOfBeds, type";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            List<Room> rooms = new LinkedList<>();
+            while(rs.next()){
+                Room room = new Room(rs.getInt("id"), rs.getInt("roomNumber"), rs.getInt("numberOfBeds"), rs.getString("type"));
+                rooms.add(room);
+            }
+            return rooms;
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                con.close();
+            }catch (SQLException throwables){
+                throwables.printStackTrace();
+            }
+        }
+        return null;
+    }
 public Room getRoom(int id){
         Connection con = null;
         try {
