ff64532721164b02f342fcb76d9e9d52b633c957
nasredd1n <122224358+nasredd1n@users.noreply.github.com>
2023-02-15 12:51:29 +0600

    Update RoomController.java

--- src/controllers/RoomController.java
+++ src/controllers/RoomController.java
@@ -9,3 +9,15 @@ public class RoomController {
     private final RoomsRepository roomsRepository;
 
 }
+public RoomController(RoomsRepository roomsRepository){
+        this.roomsRepository = roomsRepository;
+    }
+    public String getAllRooms(){
+        List<Room> rooms =roomsRepository.getAllRooms();
+        return rooms.toString();
+    }
+    public String getRoom(int id){
+        Room room = roomsRepository.getRoom(id);
+
+        return (room == null ? "Room with that id was not found" : room.toString());
+    }
